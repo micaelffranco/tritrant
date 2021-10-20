74d0bcacbfe4119c25ef2dd7825d28d252f958c4
Mick <micaelffranco@hotmail.com>
2021-10-20 06:01:11 -0400

    Added more Dummy expenses and year

--- src/App.js
+++ src/App.js
@@ -9,7 +9,11 @@ const DUMMY_EXPENSES = [
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e2',
+      title: 'New TV',
+      amount: 799.49,
+      date: new Date(2021, 2, 12) },
     {
       id: 'e3',
       title: 'Car Insurance',
@@ -21,6 +25,42 @@ const DUMMY_EXPENSES = [
       title: 'New Desk (Wooden)',
       amount: 450,
       date: new Date(2021, 5, 12),
+    },
+    {
+      id: 'e5',
+      title: 'Gym',
+      amount: 30,
+      date: new Date(2020, 2, 12),
+    },
+    {
+      id: 'e6',
+      title: 'Water',
+      amount: 200,
+      date: new Date(2021, 9, 12),
+    },
+    {
+      id: 'e7',
+      title: 'Gas',
+      amount: 130,
+      date: new Date(2021, 12, 12),
+    },
+    {
+      id: 'e8',
+      title: 'Computer',
+      amount: 2000,
+      date: new Date(2021, 10, 12),
+    },
+    {
+      id: 'e9',
+      title: 'Phone',
+      amount: 600,
+      date: new Date(2021, 1, 12),
+    },
+    {
+      id: 'e10',
+      title: 'Food',
+      amount: 200,
+      date: new Date(2021, 3, 12),
     },
   ];
 
